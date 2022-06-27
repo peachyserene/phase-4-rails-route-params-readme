77d994aa501b287a06e1f1dc38abf85df9d9aff6
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-06-27 17:48:07 -0400

    commit tree d9e8675bffc935b3fdd8b3a9bc456dcc2534d351

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,12 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
-
+  
+ def show
+      #find cheese using ID in url
+       cheese = Cheese.find_by(id: params[:id])
+      #send json response using cheese object
+      render json: cheese
+     
+    end
 end
